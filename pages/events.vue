31b5384a1d95099fc63eb0f3487538deadee5880
Filip Kuusisto Pedersen <filip.pedersen98@gmail.com>
2022-09-19 12:07:27 +0200

    get data in console

--- pages/events.vue
+++ pages/events.vue
@@ -9,6 +9,26 @@
 definePageMeta({
 	middleware: "auth",
 });
-
 const user = useSupabaseUser();
+const supabase = useSupabaseClient();
+const data = ref([]);
+const dataLoaded = ref(null);
+
+// Henter data fra Supabase
+const getData = async () => {
+	try {
+		const { data: events, error } = await supabase
+			.from("events")
+			.select("*");
+
+		if (error) throw error;
+		data.value = events;
+		dataLoaded.value = true;
+		console.log(data.value);
+	} catch (error) {
+		console.warn(error.message);
+	}
+};
+
+getData();
 </script>
