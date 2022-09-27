f9485ef849da8324ff4e19662c74ae0454388605
Filip Kuusisto Pedersen <filip.pedersen98@gmail.com>
2022-09-27 15:58:22 +0200

    tilmeld events

--- pages/events/[slug]/[id].vue
+++ pages/events/[slug]/[id].vue
@@ -7,6 +7,11 @@ const dataLoaded = ref(null);
 const statusMsg = ref(null);
 const errorMsg = ref(null);
 const route = useRoute();
+const user = supabase.auth.user();
+const currentUser = ref("");
+const eventParticipants = ref([]);
+
+const registered = ref(false);
 
 definePageMeta({
 	middleware: "auth",
@@ -19,7 +24,7 @@ const getData = async () => {
 	try {
 		const { data: events, error } = await supabase
 			.from("events")
-			.select("*")
+			.select("*, event_participants(*)")
 			.eq("id", currentId);
 		if (error) throw error;
 		data.value = events[0];
@@ -32,6 +37,82 @@ const getData = async () => {
 	}
 };
 getData();
+
+const getParticipants = async () => {
+	try {
+		const { data: participants, error } = await supabase
+			.from("event_participants")
+			.select("*")
+			.eq("event_id", currentId);
+		if (error) throw error;
+		participants.forEach(async (participant) => {
+			const user = await getUser(participant.user_id);
+			eventParticipants.value.push(user);
+			updateRegistrationStatus();
+		});
+
+		console.log(participants);
+	} catch (error) {
+		errorMsg.value = error.message;
+		setTimeout(() => {
+			errorMsg.value = false;
+		}, 5000);
+	}
+};
+getParticipants();
+
+// Henter username fra profil tabellen
+const getCurrentUser = async () => {
+	const { data: profiles } = await supabase
+		.from("profiles")
+		.select("username")
+		.eq("id", user.id);
+	currentUser.value = profiles[0].username;
+};
+getCurrentUser();
+
+const getUser = async (participant) => {
+	const { data: profiles } = await supabase
+		.from("profiles")
+		.select("username")
+		.eq("id", participant);
+	// currentUser.value = profiles[0].username;
+	console.log(profiles);
+	return profiles[0].username;
+};
+
+const updateRegistrationStatus = () => {
+	registered.value = eventParticipants.value.includes(currentUser.value);
+};
+
+// Tilmeld event
+const registerEvent = async () => {
+	if (eventParticipants.value.includes(currentUser.value)) return;
+	try {
+		const { error } = await supabase.from("event_participants").insert({
+			event_id: currentId,
+			user_id: user.id,
+		});
+		if (error) throw error;
+		getParticipants();
+	} catch (error) {
+		errorMsg.value = error.message;
+	}
+};
+
+// Afmeld event
+const cancelRegistration = async () => {
+	try {
+		const { error } = await supabase
+			.from("events")
+			.update({ participants: "" })
+			.eq("participants", user.id);
+		if (error) throw error;
+		registered.value = false;
+	} catch (error) {
+		errorMsg.value = error.message;
+	}
+};
 </script>
 
 <template>
@@ -53,7 +134,14 @@ getData();
 			<img :src="data.img_url" class="w-80 h-auto object-cover rounded" />
 			<h1>{{ data.title }}</h1>
 			<p>{{ data.description }}</p>
-			<p>Deltagere:</p>
+			<template
+				v-for="participant in eventParticipants"
+				:key="participant"
+			>
+				<p>Deltagere: {{ participant }}</p>
+			</template>
 		</div>
+		<button v-if="!registered" @click="registerEvent()">Tilmeld</button>
+		<button v-else>Afmeld</button>
 	</div>
 </template>
--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,62 +1,9 @@
 <script setup lang="ts">
-import { useDashify } from "@/composable/dashify";
-import { ref } from "vue";
 definePageMeta({
 	middleware: "auth",
 });
-const user = useSupabaseUser();
-const supabase = useSupabaseClient();
-const data = ref([]);
-const dataLoaded = ref(null);
-const { dashify } = useDashify();
-
-// Henter data fra Supabase
-const getData = async () => {
-	try {
-		const { data: events, error } = await supabase
-			.from("events")
-			.select("*");
-
-		if (error) throw error;
-		data.value = events;
-		dataLoaded.value = true;
-	} catch (error) {
-		console.warn(error.message);
-	}
-};
-getData();
 </script>
 
 <template>
-	<div>
-		<p class="text-2xl">
-			Hello
-			<span class="text-lait-yellow text-2xl">{{
-				user?.user_metadata.user_name || user?.email
-			}}</span>
-		</p>
-
-		<div v-if="dataLoaded">
-			<div
-				class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
-			>
-				<nuxt-link
-					class="flex flex-col items-center p-8 cursor-pointer bg-light-blue"
-					:to="{
-						path: `/events/${dashify(event.title)}/${event.id}`,
-					}"
-					v-for="(event, index) in data"
-					:key="index"
-				>
-					<img :src="event.img_url" class="rounded" />
-
-					<h1 class="mb-2 text-center text-xl">
-						{{ event.title }}
-					</h1>
-
-					<p class="mt-6 py-1 px-3">{{ event.description }}</p>
-				</nuxt-link>
-			</div>
-		</div>
-	</div>
+	<div></div>
 </template>
