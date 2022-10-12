302df9904a5c0f22e32cd6c975c43ffb4a2ed451
Filip Kuusisto Pedersen <filip.pedersen98@gmail.com>
2022-10-12 16:09:11 +0200

    add vue datepicker

---
+++ components/DatePicker.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
+const date = ref();
+
+const format = (date) => {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+};
+</script>
+
+<template>
+    <Datepicker
+        v-model="date"
+        :enableTimePicker="false"
+        placeholder="Vælg dato"
+        :format="format"
+    ></Datepicker>
+</template>
---
+++ components/TimePicker.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
+const time = ref({
+    hours: new Date(),
+    minutes: new Date(),
+});
+</script>
+
+<template>
+    <Datepicker
+        v-model="time"
+        timePicker
+        hideInputIcon
+        inputClassName=""
+        placeholder="Vælg tidspunkt"
+    />
+</template>
