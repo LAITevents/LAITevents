25df83b38cf175aa5319daff4e1c4160ae86cb80
FilipPedersen <60428645+FilipPedersen@users.noreply.github.com>
2022-11-13 22:09:56 +0100

    add array for tags and new filter for events

--- components/EventCard.vue
+++ components/EventCard.vue
@@ -12,45 +12,102 @@ const props = defineProps({
     loaded: { type: Boolean },
 });
 
-const toggleOnClick = (event) => {
-    const element = event.target;
-    const text = element.textContent.trim();
+const filterValue = ref("");
+const activeIndex = ref(0);
 
-    if (selected.value.includes(text)) {
-        element.classList.add("line-through");
-        selected.value.splice(selected.value.indexOf(text), 1);
-    } else {
-        element.classList.remove("line-through");
-        selected.value.push(text);
-    }
+const hashtags = ref([
+    { name: "#ALLE", count: 2 },
+    { name: "#SJOV", count: 2 },
+    { name: "#WEBINAR", count: 2 },
+    { name: "#KURSUS", count: 2 },
+    { name: "#SPORT", count: 2 },
+]);
+
+const setFilter = (hashtag: string) => {
+    filterValue.value = hashtag;
 };
 
-const filteredEvents = computed(() => {
-    if (selected.value.length == 4) {
-        return props.events;
+const setActive = (index: number) => {
+    activeIndex.value = index;
+};
+
+const filteredEvents = filterEvents(filterValue.value);
+
+function filterEvents(filterValue: string) {
+    let filteredArray = [];
+    if (activeIndex.value == 0) {
+        filteredArray = props.events;
     } else {
-        return props.events.filter(
-            (event) => !selected.value.includes(event.category_id.name)
-        );
+        filteredArray = props.events.filter((event: any) => {
+            const tags = event.category_id.name;
+            let events = null;
+            if (tags != null && tags.includes(filterValue)) {
+                events = event;
+            }
+            if (events != null) {
+                return event;
+            }
+        });
     }
-});
+    return filteredArray;
+}
+
+// const toggleOnClick = (event) => {
+//     const element = event.target;
+//     const text = element.textContent.trim();
+
+//     if (selected.value.includes(text)) {
+//         element.classList.add("line-through");
+//         selected.value.splice(selected.value.indexOf(text), 1);
+//     } else {
+//         element.classList.remove("line-through");
+//         selected.value.push(text);
+//     }
+// };
+
+// const filteredEvents = computed(() => {
+//     if (selected.value.length == 4) {
+//         return props.events;
+//     } else {
+//         return props.events.filter(
+//             (event) => !selected.value.includes(event.category_id.name)
+//         );
+//     }
+// });
 </script>
 
 <template>
-    <div v-if="loaded">
-        <div
+    <div>
+        <!-- <div
             class="flex space-x-6 space-y-2 flex-wrap lg:justify-end items-end cursor-pointer text-lait-yellow font-bold"
         >
             <div @click="toggleOnClick($event)">#SJOV</div>
             <div @click="toggleOnClick($event)">#WEBINAR</div>
             <div @click="toggleOnClick($event)">#KURSUS</div>
             <div @click="toggleOnClick($event)">#SPORT</div>
+        </div> -->
+
+        <div
+            class="flex space-x-6 space-y-2 flex-wrap lg:justify-end items-end cursor-pointer text-lait-yellow font-bold"
+        >
+            <ul>
+                <li
+                    v-for="(hashtag, index) in hashtags"
+                    :key="index"
+                    @click="[setFilter(hashtag.name), setActive(index)]"
+                >
+                    <span :class="{ 'line-through': activeIndex === index }">
+                        {{ hashtag.name }}
+                    </span>
+                    <span>{{ hashtag.count }}</span>
+                </li>
+            </ul>
         </div>
 
         <div class="grid grid-cols-1 lg:grid-cols-12 gap-8">
             <NuxtLink
-                v-for="event in filteredEvents"
-                :key="event.id"
+                v-for="(event, index) in filteredEvents"
+                :key="index + 1"
                 class="flex-col col-span-4 justify-between cursor-pointer lg:translate-event relative group"
                 :to="{
                     path: `/events/${dashify(event.title)}/${event.id}`,
