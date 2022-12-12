d0c573200422ba44fb943acd8c02bbffcec8dad9
FilipPedersen <60428645+FilipPedersen@users.noreply.github.com>
2022-12-12 10:33:03 +0100

    update mobile size

--- components/ProfileAvatar.vue
+++ components/ProfileAvatar.vue
@@ -54,7 +54,7 @@ watch(path, () => {
 
 <template>
     <label for="single" class="flex flex-col">
-        <img v-if="path" :src="src" class="object-cover h-72" />
+        <img v-if="path" :src="src" class="object-cover lg:h-72" />
         <img v-else src="@/assets/placeholder.jpg" class="h-72 object-cover" />
 
         <div v-if="props.showUpload">
