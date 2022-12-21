a2675e883f16d95e773b61a66081640b86c138f7
Filip Kuusisto Pedersen <filip.pedersen98@gmail.com>
2022-12-21 15:13:25 +0100

    test middleware logout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
+<script>
+definePageMeta({
+	middleware: "auth",
+});
+</script>
 <template>
-    <div class="mx-auto w-full px-4 sm:px-6 lg:px-16 max-w-screen-2xl">
-        <TheHeader />
-        <div>
-            <slot />
-        </div>
-    </div>
+	<div class="mx-auto w-full px-4 sm:px-6 lg:px-16 max-w-screen-2xl">
+		<TheHeader />
+		<div>
+			<slot />
+		</div>
+	</div>
 </template>
